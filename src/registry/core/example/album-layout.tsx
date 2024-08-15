9534ce427802c9a2c44972a7b3da63b0e28df467
vivianlin <linvivian61@gmail.com>
2024-08-15 16:56:54 -0400

    create player

--- src/registry/core/example/album-layout.tsx
+++ src/registry/core/example/album-layout.tsx
@@ -1,10 +1,17 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import Image from 'next/image';
+import { Heart, Play } from 'lucide-react';
+import { FastForward, SkipBack, SkipForward } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
-export default function Example() {
-  const [activeAlbum, setActiveAlbum] = useState<null | string>(null);
-  // const containerRef = useRef<HTMLDivElement>(null);
+interface Album {
+  title: string;
+  description: string;
+  longDescription: string;
+  image: string;
+}
+
+export default function SharedLayout() {
+  const [activeAlbum, setActiveAlbum] = useState<Album | null>(null);
 
   useEffect(() => {
     function onKeyDown(event: KeyboardEvent) {
@@ -16,193 +23,177 @@ export default function Example() {
     window.addEventListener('keydown', onKeyDown);
     return () => window.removeEventListener('keydown', onKeyDown);
   }, []);
+  const currentTime = 22; // seconds
+  const totalDuration = 173; // seconds (2:53)
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    return `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  };
 
   return (
-    <div className='grid place-items-center'>
-      <AnimatePresence mode='popLayout'>
-        {activeAlbum && (
+    <>
+      <AnimatePresence>
+        {activeAlbum ? (
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            key='large'
-            layoutId={`card-${'album'}`}
-            onClick={() => setActiveAlbum(null)}
-            className='flex flex-col items-center justify-center'
-          >
-            <motion.div layoutId='image-album'>
-              <Image
-                className='rounded-xl w-[300px] '
-                alt='album-cover'
+            className='overlay'
+          />
+        ) : null}
+      </AnimatePresence>
+      <AnimatePresence>
+        {activeAlbum ? (
+          <div className='absolute inset-0 grid place-items-center z-10'>
+            <motion.div
+              layoutId={`card-${activeAlbum.title}`}
+              className='flex h-fit cursor-pointer flex-col gap-4 overflow-hidden p-4'
+              style={{ borderRadius: 12 }}
+            >
+              <motion.img
+                layoutId={`image-${activeAlbum.title}`}
                 height={300}
                 width={300}
-                src='https://i.scdn.co/image/ab67616d0000b27371d62ea7ea8a5be92d3c1f62'
+                alt='Album'
+                src={activeAlbum.image}
+                className='mx-auto p-4'
+                style={{ borderRadius: 24 }}
               />
-            </motion.div>
+              <div className='flex items-center justify-between'>
+                <div className='flex flex-col items-start gap-1'>
+                  <div className='flex w-full justify-between'>
+                    <motion.p
+                      layoutId={`title-${activeAlbum.title}`}
+                      className='text-xl font-semibold text-white'
+                    >
+                      {activeAlbum.title}
+                    </motion.p>
+                  </div>
+                  <div className='flex flex-col text-left'>
+                    <motion.p
+                      layoutId={`description-${activeAlbum.description}`}
+                      className='text-base font-medium text-gray-400'
+                    >
+                      {activeAlbum.description}
+                    </motion.p>
+                  </div>
+                </div>
+                <div className='flex gap-4'>
+                  <Heart />
+                </div>
+              </div>
 
-            <div className='flex flex-col gap-1 px-1 text-left w-full'>
-              <motion.h2
-                layoutId='title-album'
-                className='text-lg font-semibold'
-              >
-                HIT ME HARD AND SOFT
-              </motion.h2>
-              <motion.p
-                layoutId='description-album'
-                className='text-base text-gray-600'
-              >
-                Billie Ellish
-              </motion.p>
-            </div>
+              <div className='w-full mt-2'>
+                <div className='bg-gray-600 rounded-md h-0.5 relative'>
+                  <div
+                    className='bg-white rounded-md h-0.5'
+                    style={{ width: `${(currentTime / totalDuration) * 100}%` }}
+                  ></div>
+                  <div
+                    className='absolute bg-white rounded-full w-3 h-3'
+                    style={{
+                      left: `${(currentTime / totalDuration) * 100}%`,
+                      top: '50%',
+                      transform: 'translate(-50%, -50%)',
+                    }}
+                  ></div>
+                </div>
+                <div className='flex justify-between text-zinc-400 text-xs font-normal leading-3 mt-2'>
+                  <div>{formatTime(currentTime)}</div>
+                  <div>{formatTime(totalDuration)}</div>
+                </div>
+              </div>
 
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.1 }}
-            >
-              <button className='px-2 bg-red-200'>play button</button>
+              <motion.div
+                layout
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0, transition: { duration: 0.05 } }}
+                className='flex flex-col w-full items-center bg-black rounded-2xl'
+              >
+                <div className='flex w-full justify-center items-center'>
+                  <div className='w-full flex justify-between items-center mt-4'>
+                    <FastForward
+                      className='transform rotate-180 text-gray-500 w-6 h-6 cursor-pointer'
+                      fill='gray'
+                    />
+                    <SkipBack
+                      fill='white'
+                      className='text-white w-6 h-6 cursor-pointer'
+                    />
+                    <div className='bg-white p-3 rounded-full'>
+                      <Play
+                        fill='black'
+                        className='text-white w-8 h-8 cursor-pointer'
+                      />
+                    </div>
+
+                    <SkipForward
+                      fill='white'
+                      className='text-white w-6 h-6 cursor-pointer'
+                    />
+                    <FastForward
+                      className='text-gray-500 w-6 h-6 cursor-pointer'
+                      fill='gray'
+                    />
+                  </div>
+                </div>
+              </motion.div>
             </motion.div>
-          </motion.div>
-        )}
+          </div>
+        ) : null}
       </AnimatePresence>
 
-      <motion.div
-        layoutId={`card-${'album'}`}
-        key='small'
-        onClick={() => setActiveAlbum('album')}
-      >
-        <motion.div layoutId='image-album'>
-          <Image
-            className='rounded-xl w-[160px] '
-            alt='album-cover'
-            height={160}
-            width={160}
-            src='https://i.scdn.co/image/ab67616d0000b27371d62ea7ea8a5be92d3c1f62'
-          />
-        </motion.div>
-
-        <div className='flex-grow text-left px-2 mt-2'>
-          <motion.h2 layoutId='title-album' className='text-xs font-semibold'>
-            HIT ME HARD AND SOFT
-          </motion.h2>
-          <motion.p
-            layoutId='description-album'
-            className='text-sm text-gray-600'
+      <ul className='relative flex w-full flex-col items-center p-0 my-12'>
+        {GAMES.map((game) => (
+          <motion.li
+            layoutId={`card-${game.title}`}
+            key={game.title}
+            onClick={() => setActiveAlbum(game)}
+            className='flex flex-col w-96 cursor-pointer items-center gap-2 p-0 rounded-lg'
           >
-            Billie Ellish
-          </motion.p>
-        </div>
-      </motion.div>
-    </div>
+            <motion.img
+              layoutId={`image-${game.title}`}
+              style={{ borderRadius: 12 }}
+              height={180}
+              width={180}
+              alt='Album'
+              src={game.image}
+            />
+            <div className='flex flex-grow items-center justify-between border-b border-gray-200 last:border-none'>
+              <div className='flex flex-col items-center gap-1 '>
+                <div className='flex flex-col'>
+                  <motion.p
+                    layoutId={`title-${game.title}`}
+                    className='text-base font-semibold text-white'
+                  >
+                    {game.title}
+                  </motion.p>
+                </div>
+                <div className='flex flex-col '>
+                  <motion.p
+                    layoutId={`description-${game.description}`}
+                    className='text-sm font-semibold text-white'
+                  >
+                    {game.description}
+                  </motion.p>
+                </div>
+              </div>
+            </div>
+          </motion.li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-// import { AnimatePresence, motion } from 'framer-motion';
-// import Image from 'next/image';
-// import React, { useEffect, useRef, useState } from 'react';
-
-// interface AlbumCardProps {}
-
-// export const AlbumCard: React.FC<AlbumCardProps> = ({}) => {
-//   const [activeAlbum, setActiveAlbum] = useState<null | string>(null);
-//   const containerRef = useRef<HTMLDivElement>(null);
-
-//   useEffect(() => {
-//     function onKeyDown(event: KeyboardEvent) {
-//       if (event.key === 'Escape') {
-//         setActiveAlbum(null);
-//       }
-//     }
-
-//     window.addEventListener('keydown', onKeyDown);
-//     return () => window.removeEventListener('keydown', onKeyDown);
-//   }, []);
-
-//   return (
-//     <>
-//       <AnimatePresence>
-//         {activeAlbum ? (
-//           <motion.div
-//             initial={{ opacity: 0 }}
-//             animate={{ opacity: 1 }}
-//             exit={{ opacity: 0 }}
-//             className='flex items-center justify-center bg-black bg-opacity-50'
-//           >
-//             <motion.div
-//               ref={containerRef}
-//               layoutId={`card-${'album'}`}
-//               transition={{
-//                 layout: {
-//                   duration: 0.5,
-//                   type: 'spring',
-//                   bounce: 0,
-//                 },
-//               }}
-//               onClick={(e) => e.stopPropagation()}
-//               className='flex flex-col rounded-xl gap-2 text-base text-gray-100 cursor-pointer bg-white'
-//             >
-//               <motion.div layoutId='image-album'>
-//                 <Image
-//                   className='rounded-xl w-[300px] '
-//                   alt='album-cover'
-//                   height={300}
-//                   width={300}
-//                   src={
-//                     'https://i.scdn.co/image/ab67616d0000b27371d62ea7ea8a5be92d3c1f62'
-//                   }
-//                 />
-//               </motion.div>
-
-//               <div className='flex flex-col gap-1 px-1'>
-//                 <motion.div layoutId={`title-${'album'}`} className='text-sm'>
-//                   HIT ME HARD AND SOFT
-//                 </motion.div>
-//                 <motion.div
-//                   layoutId={`artist-${'album'}`}
-//                   className='text-xs opacity-80'
-//                 >
-//                   Billie Ellish
-//                 </motion.div>
-//               </div>
-//             </motion.div>
-//           </motion.div>
-//         ) : (
-//           <motion.div
-//             layoutId={`card-${'album'}`}
-//             key={'album'}
-//             onClick={() => setActiveAlbum('album')}
-//             className='flex items-center justify-center bg-black bg-opacity-50'
-//           >
-//             <motion.div className='flex flex-col rounded-xl gap-2 text-base text-gray-100 cursor-pointer bg-white'>
-//               <motion.div layoutId={`image-album`}>
-//                 <Image
-//                   className='rounded-xl'
-//                   alt='album-cover'
-//                   height={180}
-//                   width={180}
-//                   src={
-//                     'https://i.scdn.co/image/ab67616d0000b27371d62ea7ea8a5be92d3c1f62'
-//                   }
-//                 />
-//               </motion.div>
-
-//               <div className='flex flex-col gap-1 px-1'>
-//                 <motion.div layoutId={`title-${'album'}`} className='text-sm'>
-//                   HIT ME HARD AND SOFT
-//                 </motion.div>
-//                 <motion.div
-//                   layoutId={`artist-${'album'}`}
-//                   className='text-xs opacity-80'
-//                 >
-//                   Billie Ellish
-//                 </motion.div>
-//               </div>
-//             </motion.div>
-//           </motion.div>
-//         )}
-//       </AnimatePresence>
-//     </>
-//   );
-// };
-// export default AlbumCard;
+const GAMES = [
+  {
+    title: 'Billie Ellish',
+    description: 'THE GREATEST',
+    longDescription: 'The song',
+    image: 'https://i.scdn.co/image/ab67616d0000b27371d62ea7ea8a5be92d3c1f62',
+  },
+];
