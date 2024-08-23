f02cbffc1bcafd04059f3168f2f96afb77515c50
vivianlin <linvivian61@gmail.com>
2024-08-23 13:07:39 -0400

    feat(calendar): add animations

--- src/registry/core/ui/calendar.tsx
+++ src/registry/core/ui/calendar.tsx
@@ -10,9 +10,9 @@ import {
   startOfWeek,
   subMonths,
 } from 'date-fns';
+import { AnimatePresence, motion, MotionConfig } from 'framer-motion';
 import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
 import { useState } from 'react';
-
 export default function Page() {
   const [monthString, setMonthString] = useState(format(new Date(), 'yyyy-MM'));
   const month = parse(monthString, 'yyyy-MM', new Date());
@@ -35,50 +35,61 @@ export default function Page() {
   });
 
   return (
-    <div className='flex w-full items-start bg-stone-800 py-16 text-stone-900'>
-      <div className='mx-auto w-full max-w-md rounded-2xl bg-white'>
-        <div className='py-8'>
-          <div className='flex flex-col justify-center rounded text-center'>
-            <header className='relative flex justify-between px-8'>
-              <button
-                className='z-10 rounded-full p-1.5 hover:bg-stone-100'
-                onClick={previousMonth}
-              >
-                <ChevronLeftIcon className='h-4 w-4' />
-              </button>
-              <p className='absolute inset-0 flex items-center justify-center font-semibold'>
-                {format(month, 'MMMM yyyy')}
-              </p>
-              <button
-                className='z-10 rounded-full p-1.5 hover:bg-stone-100'
-                onClick={nextMonth}
-              >
-                <ChevronRightIcon className='h-4 w-4' />
-              </button>
-            </header>
-
-            <div className='mt-6 grid grid-cols-7 gap-y-6 px-8 text-sm'>
-              <span className='font-medium text-stone-500'>Su</span>
-              <span className='font-medium text-stone-500'>Mo</span>
-              <span className='font-medium text-stone-500'>Tu</span>
-              <span className='font-medium text-stone-500'>We</span>
-              <span className='font-medium text-stone-500'>Th</span>
-              <span className='font-medium text-stone-500'>Fr</span>
-              <span className='font-medium text-stone-500'>Sa</span>
-              {days.map((day) => (
-                <span
-                  className={`${
-                    isSameMonth(day, month) ? '' : 'text-stone-300'
-                  } font-semibold`}
-                  key={format(day, 'yyyy-MM-dd')}
+    <MotionConfig transition={{ duration: 1 }}>
+      <div className='flex w-full items-start bg-stone-800 py-16 text-stone-900'>
+        <div className='relative mx-auto w-full max-w-md  overflow-hidden rounded-2xl bg-white'>
+          <div className='py-8'>
+            <div className='flex flex-col justify-center rounded text-center'>
+              <AnimatePresence mode='popLayout' initial={false}>
+                <motion.div
+                  key={monthString}
+                  initial={{ x: '100%' }}
+                  animate={{ x: '0%' }}
+                  exit={{ x: '-100%' }}
                 >
-                  {format(day, 'd')}
-                </span>
-              ))}
+                  <header className='relative flex justify-between px-8'>
+                    <button
+                      className='z-10 rounded-full p-1.5 hover:bg-stone-100'
+                      onClick={previousMonth}
+                    >
+                      <ChevronLeftIcon className='h-4 w-4' />
+                    </button>
+                    <p className='absolute inset-0 flex items-center justify-center font-semibold'>
+                      {format(month, 'MMMM yyyy')}
+                    </p>
+                    <button
+                      className='z-10 rounded-full p-1.5 hover:bg-stone-100'
+                      onClick={nextMonth}
+                    >
+                      <ChevronRightIcon className='h-4 w-4' />
+                    </button>
+                  </header>
+
+                  <div className='mt-6 grid grid-cols-7 gap-y-6 px-8 text-sm'>
+                    <span className='font-medium text-stone-500'>Su</span>
+                    <span className='font-medium text-stone-500'>Mo</span>
+                    <span className='font-medium text-stone-500'>Tu</span>
+                    <span className='font-medium text-stone-500'>We</span>
+                    <span className='font-medium text-stone-500'>Th</span>
+                    <span className='font-medium text-stone-500'>Fr</span>
+                    <span className='font-medium text-stone-500'>Sa</span>
+                    {days.map((day) => (
+                      <span
+                        className={`${
+                          isSameMonth(day, month) ? '' : 'text-stone-300'
+                        } font-semibold`}
+                        key={format(day, 'yyyy-MM-dd')}
+                      >
+                        {format(day, 'd')}
+                      </span>
+                    ))}
+                  </div>
+                </motion.div>
+              </AnimatePresence>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </MotionConfig>
   );
 }
