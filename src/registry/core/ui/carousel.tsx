f06e60c5dc5f808d445f93224d53dfff19d7b65c
vivianlin <linvivian61@gmail.com>
2024-09-15 08:40:17 -0400

    feat(carousel): add thumbnail animations

--- src/registry/core/ui/carousel.tsx
+++ src/registry/core/ui/carousel.tsx
@@ -1,33 +1,52 @@
 import { AnimatePresence, motion, MotionConfig } from 'framer-motion';
 import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const images = [
   'https://images.unsplash.com/photo-1725267385461-cab515fc1bbe?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1593642532973-d31b6557fa68?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1725267385461-cab515fc1bbe?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1593642634367-d91a135587b5?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 ];
+const collapsedAspectRatio = 1 / 3;
+const fullAspectRatio = 3 / 2;
+const gap = 2;
+const margin = 12;
 
 const Carousel: React.FC = () => {
   const [index, setIndex] = useState(0);
+  useEffect(() => {
+    function handleKeyPress(e: KeyboardEvent) {
+      if (e.key === 'ArrowLeft') {
+        if (index > 0) {
+          setIndex(index - 1);
+        }
+      } else if (e.key === 'ArrowRight') {
+        if (index < images.length - 1) {
+          setIndex(index + 1);
+        }
+      }
+    }
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [index]);
 
   return (
-    <MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
+    <MotionConfig transition={{ duration: 0.8, ease: [0.32, 0.72, 0, 1] }}>
       <div className='h-full bg-black'>
         <div className='mx-auto flex h-full max-w-7xl flex-col justify-center'>
           <div className='relative overflow-hidden'>
-            <motion.div
-              animate={{ x: `-${index * 100}%` }}
-              transition={{ duration: 0.5, ease: 'circOut' }}
-              className='flex'
-            >
+            <motion.div animate={{ x: `-${index * 100}%` }} className='flex'>
               {images.map((image, i) => (
-                <img
+                <motion.img
                   key={image}
                   src={image}
                   alt='image'
+                  animate={{ opacity: i === index ? 1 : 0.4 }}
                   className='aspect-[3/2] object-cover'
                 />
               ))}
@@ -60,6 +79,46 @@ const Carousel: React.FC = () => {
                 </motion.button>
               )}
             </AnimatePresence>
+          </div>
+          <div className='absolute inset-x-0 bottom-6 flex justify-center overflow-hidden'>
+            <motion.div
+              initial={false}
+              animate={{
+                x: `-${
+                  index * 100 * (collapsedAspectRatio / fullAspectRatio) +
+                  index * gap +
+                  margin
+                }%`,
+              }}
+              style={{ aspectRatio: fullAspectRatio, gap: `${gap}%` }}
+              className='flex h-14'
+            >
+              {images.map((image, i) => (
+                <motion.button
+                  key={image}
+                  onClick={() => setIndex(i)}
+                  whileHover={{ opacity: 1 }}
+                  initial={false}
+                  animate={i === index ? 'active' : 'inactive'}
+                  variants={{
+                    active: {
+                      marginLeft: `${margin}%`,
+                      marginRight: `${margin}%`,
+                      opacity: 1,
+                      aspectRatio: fullAspectRatio,
+                    },
+                    inactive: {
+                      marginLeft: '0%',
+                      marginRight: '0%',
+                      opacity: 0.5,
+                      aspectRatio: collapsedAspectRatio,
+                    },
+                  }}
+                >
+                  <motion.img src={image} className='h-full object-cover' />
+                </motion.button>
+              ))}
+            </motion.div>
           </div>
         </div>
       </div>
